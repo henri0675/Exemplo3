a6847e2c82394e6ae3a38afdfa9f28ea09a7b975
Henrique <henriinfanti@gmail.com>
2025-03-31 09:08:11 -0300

    declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIAVEIS
+
+var nome ="fiap";
+console.log(nome);
+
+let idade = 18;
+console.log(idade)
+
+const sobrenome="Fiapinho";
+console.log(sobrenome)
+
+//declarando um variavel indefinida
+let nome1;
+console.log(nome1)
+
+//declarando uma variavel nula
+let nome2=null;
+console.log(nome2)
+
